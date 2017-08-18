00e8b3329c1971fa7a23f2e8fcf90247ed067425
Fernando <luisfernandoguevara95@gmail.com>
2017-08-18 00:56:50 -0500

    Se agregó la base del diseño inicial

--- src/posts/views.py
+++ src/posts/views.py
@@ -5,5 +5,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def post_home(request):
-	return HttpResponse("<h1>Hola mundo</h1>")
+def post_create(request):
+	return HttpResponse("<h1>Crear</h1>")
+
+def post_detail(request):
+	return HttpResponse("<h1>Ver</h1>")
+
+def post_list(request):
+	return render(request, "home.html", {})
+
+def post_update(request):
+	return HttpResponse("<h1>Modificar</h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1>Eliminar</h1>")
