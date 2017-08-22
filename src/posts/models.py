4d7c12f83b93af66a5f11efec138d6940c8a2182
Fernando <luisfernandoguevara95@gmail.com>
2017-08-22 17:01:49 -0500

    Adición de libreria Pillow para manejo de imágenes

--- src/posts/models.py
+++ src/posts/models.py
@@ -5,15 +5,24 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
+def upload_location(instance, filename):
+	return "%s/%s" %(instance.id, filename)
+
 class Post(models.Model):
 	title = models.CharField(max_length=50)
-	image = models.FileField(null=True, blank=True)
+	image = models.ImageField(upload_to=upload_location,
+		null=True, 
+		blank=True,
+		height_field="height_field",
+		width_field="width_field")
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	description = models.TextField(max_length=256)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	update = models.DateTimeField(auto_now_add=False, auto_now=True)
 
 	def __unicode__(self):
-		return self.name
+		return self.title
 
 	def get_absolute_url(self):
 		return reverse("posts:detail", kwargs={"id": self.id})		
