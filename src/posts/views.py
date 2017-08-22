ccc903212740431afd62a53ad8f5f35be060316e
Fernando <luisfernandoguevara95@gmail.com>
2017-08-22 16:24:33 -0500

    Adición de pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import messages 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -31,11 +32,26 @@ def post_detail(request, id=None):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all()
+
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get('page_request_var')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 	context = {
 		"title": "Wine Cellar",
-		"obj_list": queryset
-	}
+		"obj_list": queryset,
+		"page_request_var": page_request_var
+	}    
+
 	return render(request, "home.html", context)
 
 def post_update(request, id=None):
