3dbcc7e53e0b537a20ab5c6cf58b4b2ce80662c5
Fernando <luisfernandoguevara95@gmail.com>
2017-10-09 15:28:25 -0500

    Mejora en la vista de las Cards

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.conf import settings 
 from django.core.urlresolvers import reverse
 
 from django.db import models
@@ -9,6 +10,7 @@ def upload_location(instance, filename):
 	return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	title = models.CharField(max_length=50)
 	image = models.ImageField(upload_to=upload_location,
 		null=True, 
